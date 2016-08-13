68ba8adb73df4a2d60329936c4da926921fa5af4
SonLT <sonlt@vncomputing.com>
2016-08-14 00:03:59 +0700

    Delete custom code, use yo

--- modules/farms/client/farms.client.module.js
+++
@@ -1,8 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('farms', ['core']);
-})(ApplicationConfiguration);
-
-ApplicationConfiguration.registerModule('farms.admin', ['core.admin']);
-ApplicationConfiguration.registerModule('farms.admin.routes', ['core.admin.routes']);
