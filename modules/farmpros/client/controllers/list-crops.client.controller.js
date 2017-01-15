673a6a4b42bebd93d998685bd66b839fb9ffa103
Steven Lam <dewn49@gmail.com>
2017-01-16 01:07:52 +0700

    Done: calc yield; Todo: search by product

--- modules/farmpros/client/controllers/list-crops.client.controller.js
+++ modules/farmpros/client/controllers/list-crops.client.controller.js
@@ -5,10 +5,51 @@
     .module('farmpros')
     .controller('CropsListController', CropsListController);
 
-  CropsListController.$inject = ['CropsService'];
+  CropsListController.$inject = ['CropsService', 'HarvestListService', '$q'];
 
-  function CropsListController(CropsService) {
+  function CropsListController(CropsService, HarvestListService, $q) {
     var vm = this;
     vm.crops = CropsService.query();
+    vm.arrYielPlan = [];
+    vm.arrYielActual = [];
+    var i=0;
+
+    // $q.all(vm.crops).then((crs) => {
+    //     console.log(crs[0]); // value alpha
+    //     console.log(crs[1]); // value betaa
+    //     complete();
+    // });
+
+    vm.crs = [];
+    vm.crops.$promise.then(function(crops) {
+      console.log('q.all done, return size: ' + crops.length);
+      vm.crs = crops;
+
+      for (var j in vm.crs) {
+          if (j < vm.crs.length) {
+            var crop = vm.crs[j];
+            vm.arrYielPlan.push(0); 
+            vm.arrYielActual.push(0);
+            // console.log('Crop #' + j + ' id = ' + crop._id);
+            getYielOfCrop(crop._id, vm.arrYielPlan.length-1);  
+          }
+      }    
+
+    });
+
+    function getYielOfCrop(iCropId, index) {
+      var harvests = [];            
+      HarvestListService.selectFromCrop({cropId: iCropId}).$promise.then(function(hvs) {
+        harvests = hvs;
+        // console.log(harvests);
+        for (var k in harvests) {
+          if (k < harvests.length) {
+            vm.arrYielPlan[index] += parseInt(harvests[k].planyield, 10);
+            vm.arrYielActual[index] += parseInt(harvests[k].harvestyield, 10);
+          }
+        }
+      });
+    }
+
   }
 }());
